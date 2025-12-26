feb4438a1883c7c99a4694db444f24be76755027
wxt <wb.wangxiaoteng01@mesg.corp.netease.com>
2025-12-26 18:06:36 +0800

    Fix update function - handle undefined values and use correct database fields

--- server/models/pokemonModel.js
+++ server/models/pokemonModel.js
@@ -15,24 +15,54 @@ export async function getPokemonById(id) {
 
 // 创建新的 Pokemon
 export async function createPokemon(pokemonData) {
-	const { name, type, level, hp, attack, defense } = pokemonData;
+	const { name, type, hp, attack, defense, speed } = pokemonData;
 	const sql = `
-    INSERT INTO pokemons (name, type, level, hp, attack, defense)
+    INSERT INTO pokemons (name, type, hp, attack, defense, speed)
     VALUES (?, ?, ?, ?, ?, ?)
   `;
-	const result = await query(sql, [name, type, level, hp, attack, defense]);
+	const result = await query(sql, [name, type, hp, attack, defense, speed]);
 	return result.insertId;
 }
 
 // 更新 Pokemon
 export async function updatePokemon(id, pokemonData) {
-	const { name, type, level, hp, attack, defense } = pokemonData;
-	const sql = `
-    UPDATE pokemons 
-    SET name = ?, type = ?, level = ?, hp = ?, attack = ?, defense = ?
-    WHERE id = ?
-  `;
-	await query(sql, [name, type, level, hp, attack, defense, id]);
+	// 过滤掉 undefined 值，只更新提供的字段
+	const fields = [];
+	const values = [];
+
+	if (pokemonData.name !== undefined) {
+		fields.push("name = ?");
+		values.push(pokemonData.name);
+	}
+	if (pokemonData.type !== undefined) {
+		fields.push("type = ?");
+		values.push(pokemonData.type);
+	}
+	if (pokemonData.hp !== undefined) {
+		fields.push("hp = ?");
+		values.push(pokemonData.hp);
+	}
+	if (pokemonData.attack !== undefined) {
+		fields.push("attack = ?");
+		values.push(pokemonData.attack);
+	}
+	if (pokemonData.defense !== undefined) {
+		fields.push("defense = ?");
+		values.push(pokemonData.defense);
+	}
+	if (pokemonData.speed !== undefined) {
+		fields.push("speed = ?");
+		values.push(pokemonData.speed);
+	}
+
+	if (fields.length === 0) {
+		throw new Error("没有要更新的字段");
+	}
+
+	const sql = `UPDATE pokemons SET ${fields.join(", ")} WHERE id = ?`;
+	values.push(id);
+
+	await query(sql, values);
 	return true;
 }
 
